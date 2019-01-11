ad2b5933c8a3f580d429d43feaa6fbee3e9df524
Daniel Caujolle-Bert <daniel.caujolle-bert@unibas.ch>
2019-01-11 08:40:05 +0100

    Small tweak with Y position.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -81,7 +81,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(F1RMB, 1.1.19 [005])" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(F1RMB, 1.1.19 [006])" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -902,7 +902,7 @@
 #define Y_BED_SIZE 270
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -5
+#define X_MIN_POS -10
 #define Y_MIN_POS -20
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
